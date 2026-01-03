9b666bb50f7388a256bd1fd990399164068c175d
Ashutosh Kumar <kashu2611@gmail.com>
2026-01-04 03:30:31 +0530

    Time: 0 ms (100.00%) | Memory: 30.2 MB (85.27%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,12 +1,32 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
+        // brute force approach - T O(nlogn)
         // square each number
         // sort the array
-        for(int i = 0 ; i < nums.size();i++ ){
-            nums[i] = nums[i]*nums[i];
+        // for(int i = 0 ; i < nums.size();i++ ){
+        //     nums[i] = nums[i]*nums[i];
+        // }
+        // sort(nums.begin(),nums.end());
+        // return nums;
+
+        // optimised two pointer approach T-O(n) S-O(n)
+        // take 2 pointers, l and r placed at extreme ends
+        // compare the absolute value and place it in new array
+        int n = nums.size();
+        vector<int> ans(n,0);
+        int l = 0, r = n-1, k = n-1;
+        while(l<=r){
+            if(abs(nums[l]) > abs(nums[r])){
+                ans[k] = nums[l]*nums[l];
+                l++;
+            }else {
+                ans[k] = nums[r] * nums[r];
+                r--;
+            }
+            k--;
         }
-        sort(nums.begin(),nums.end());
-        return nums;
+        
+        return ans;
     }
 };
